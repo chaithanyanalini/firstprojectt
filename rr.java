a464284af34d5f5894ed9a2a05fbe5e5f9fa8696
chaithanyanalini <48897148+chaithanyanalini@users.noreply.github.com>
2021-10-19 13:10:11 +0530

    2nd

--- rr.java
+++ rr.java
@@ -7,7 +7,9 @@ char ch1 = 'c';
 char ch2 = 'b';  
 // variable that stores the integer value of the character  
 int asciivalue1 = ch1;  
-int asciivalue2 = ch2;  
+int asciivalue2 = ch2;
+  System.out.printlan("output");
+                    
 System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
 System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
 }  
